615cffbbfe62341a4782a1810821ba53ba6e4804
Teodev578 <kfabient@gmail.com>
2026-05-19 12:13:18 +0000

    wxdfcgvhb

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Contact from "@/components/Contact";
+import Contact from "./_components/Contact";
 
 export default function ContactPage() {
   return (
